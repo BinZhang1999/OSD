cbb08df8d3dd60872fbba360aed5baacb4d35667
BinZhang <73570072+BinZhang1999@users.noreply.github.com>
2021-09-15 20:02:55 +0800

    Update main.m

--- main.m
+++ main.m
@@ -2,20 +2,20 @@
 r=4;m=7;
 k = 35; n=128;
 %% Generate Code
-% code = CODE_RM;
-% code = code.Init(r,m);
-code = CODE_RANDOM;
-code = code.Init(k,n);
+code = CODE_RM;
+code = code.Init(r,m);
+% code = CODE_RANDOM;
+% code = code.Init(k,n);
 
 %% Generate Decoder
 decoder = DECODER_AWGN_OSD;
-decoder = decoder.Init(G, 4, 3); % osd-2
+decoder = decoder.Init(code.G, 8, 2); % osd-2
 
 
 %% Simulation Settings
 % over awgn channel
-G = G;
-simulationSetting.EbNoArray = 2.5:0.25:4.0;
+G = code.G;
+simulationSetting.EbNoArray = 3.5:0.25:5.0;
 simulationSetting.MIN_NUM_ERROR_FRAME = 100;
 simulationSetting.displayName = 'displayName = ''osd-2'';';
 simulationSetting.description = '% OSD-2 Algorithm';
