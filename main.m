b8a9d4eb96eb4cfd0b0067bb6034465934aff478
BinZhang <73570072+BinZhang1999@users.noreply.github.com>
2022-02-21 11:36:39 +0800

    :)

--- main.m
+++ main.m
@@ -1,24 +1,24 @@
 %% Code parameters
 r=4;m=7;
-k = 35; n=128;
+% k = 35; n=128;
 %% Generate Code
-% code = CODE_RM;
-% code = code.Init(r,m);
-code = CODE_RANDOM;
-code = code.Init(k,n);
-
+code = CODE_RM;
+code = code.Init(r,m);
+% code = CODE_RANDOM;
+% code = code.Init(k,n);
+G = code.G;
 %% Generate Decoder
 decoder = DECODER_AWGN_OSD;
-decoder = decoder.Init(G, 4, 4); % osd-2
+decoder = decoder.Init(G, 8, 2); % osd-2
 
 
 %% Simulation Settings
 % over awgn channel
-G = G;
-simulationSetting.EbNoArray = 1.5:0.25:2.5;
+
+simulationSetting.EbNoArray = 3.5:0.25:5.0;
 simulationSetting.MIN_NUM_ERROR_FRAME = 100;
 simulationSetting.displayName = 'displayName = ''osd-4 projected code'';';
 simulationSetting.description = '% OSD-2 Algorithm';
-simulationResult = parSimulationAWGN(simulationSetting, G, decoder);
-% simulationResult = simulationAWGN(simulationSetting, G, decoder);
+% simulationResult = parSimulationAWGN(simulationSetting, G, decoder);
+simulationResult = simulationAWGN(simulationSetting, G, decoder);
 
